301d5c5acb9a94319847e8d7194b72ef19abe71f
ivaan1992 <ivaan.lg.92@gmail.com>
2021-12-16 11:22:48 -0600

    Fix linters issues

--- src/index.js
+++ src/index.js
@@ -1,19 +1,19 @@
 import './style.css';
 
 class Score {
-    constructor(name, score, index) {
-        this.name = name;
-        this.score = score;
-        this.index = index;
-    }
+  constructor(name, score, index) {
+    this.name = name;
+    this.score = score;
+    this.index = index;
+  }
 
-    addScore() {
-        const makeList = document.getElementById('score');
-        const makeUl = document.createElement('ul');
-        makeList.appendChild(makeUl);
-        makeUl.className = 'ulList';
-        makeUl.innerHTML += `<li>${this.name} , ${this.score}</li>`
-    }
+  addScore() {
+    const makeList = document.getElementById('score');
+    const makeUl = document.createElement('ul');
+    makeList.appendChild(makeUl);
+    makeUl.className = 'ulList';
+    makeUl.innerHTML += `<li>${this.name} , ${this.score}</li>`;
+  }
 }
 const score1 = new Score('Ivan', 100, 0);
 const score2 = new Score('Ismael', 200, 1);
